61ac7e88017b8b8949e59fcb07d00e0fbf9a43b2
Marek Migas <migas@nonstop.studio>
2024-01-11 11:20:15 +0100

    fixed apos

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const projects = await getProjects();
   return (
     <div>
-      <h1 className="text-7xl font-extrabold">Hello I'm <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Marek</span></h1>
+      <h1 className="text-7xl font-extrabold">Hello I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Marek</span></h1>
       <p className="mt-3 text-xl text-gray-600">Hallo! Check out my projects</p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My projects</h2>
